7dbea930a1e2680c3eda207f499b3236e365784f
h01m3s <lin426797@gmail.com>
2018-03-01 20:02:37 -0500

    gitignore added

---
+++ IntervalTimer/TimerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntervalTimer
+//
+//  Created by Weijie Lin on 3/1/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
+
